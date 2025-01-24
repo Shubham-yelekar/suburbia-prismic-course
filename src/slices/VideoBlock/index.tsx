a1b9ef1a6fa278af6868f12b800f0df715774d9e
shubham <shubhamyelekar0@gmail.com>
2025-01-24 13:10:22 +0530

    video block done

--- src/slices/VideoBlock/index.tsx
+++ src/slices/VideoBlock/index.tsx
@@ -1,7 +1,11 @@
-import { Content } from "@prismicio/client";
+"use client";
+import { Content, isFilled } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
 import { Bounded } from "@/components/Bounded";
+import { LazyYouTubePlayer } from "./LazyYoutubePlayer";
+import clsx from "clsx";
+import Image from "next/image";
 
 /**
  * Props for `VideoBlock`.
@@ -11,6 +15,9 @@ export type VideoBlockProps = SliceComponentProps<Content.VideoBlockSlice>;
 /**
  * Component for "VideoBlock" Slices.
  */
+
+const MASK_CLASSES =
+  "[mask-image:url(/video-mask.png)] [mask-mode:alpha] [mask-position:center_center] [mask-repeat:no-repeat] [mask-size:100%_auto]";
 const VideoBlock = ({ slice }: VideoBlockProps): JSX.Element => {
   return (
     <Bounded
@@ -19,7 +26,37 @@ const VideoBlock = ({ slice }: VideoBlockProps): JSX.Element => {
       className="bg-texture bg-zinc-900"
     >
       <h2 className="sr-only">Video Reel</h2>
-      <div className="relative aspect-video"></div>
+      <div className="relative aspect-video">
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-brand-lime absolute inset-0 ~translate-x-2/3 ~translate-y-2/3 "
+          )}
+        ></div>
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/3 ~translate-y-1/2"
+          )}
+        ></div>
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/2 ~-translate-y-1/3 "
+          )}
+        ></div>
+        <div className={clsx(MASK_CLASSES, "relative h-full")}>
+          {isFilled.keyText(slice.primary.youtube_video_id) ? (
+            <LazyYouTubePlayer youTubeID={slice.primary.youtube_video_id} />
+          ) : null}
+          <Image
+            src="/image-texture.png"
+            fill
+            alt=""
+            className="pointer-events-none object-cover opacity-50"
+          />
+        </div>
+      </div>
     </Bounded>
   );
 };
